042214c14464889489d5cf256ee69f626ac978b4
DevinColon <97969346+DevinColon@users.noreply.github.com>
2022-02-03 10:38:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homers-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+$(".reply-yes").click(function() {
+    $(".simpson").hide();
+});
+
+//Independent Practice
+$(".reply-no").click(function() {  
+  $(".No").text("Doh!");
+    $(".No").css("color","Orange");
+    $("img").css("width", "400px", "height","400px");
+    $("h1").css("color","Purple");
+});
+
